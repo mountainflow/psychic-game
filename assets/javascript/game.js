93d1da70399edee19191ff0e1e710add423a466c
mountainflow <46331541+mountainflow@users.noreply.github.com>
2019-02-20 17:42:54 -0700

    put everything in function userGuess

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,34 +4,37 @@ var losses = 0;
 var guessesLeft = 9;
 var guessesSoFar;
 
+
 // computer picks random letters
 function computerGuess() {
     return Math.floor(Math.random() * 26) + 65;
 };
 
 var computerChoice = String.fromCharCode(computerGuess());
+console.log("Computer's Guess: " + computerChoice);
 
 // user chooses letter
 document.addEventListener("keyup", userGuess);
 
-function userGuess(event) {
+function userGuess() {
     var theKey = event.key.toUpperCase();
     console.log("Your Guess: " + theKey);
+
+    // compare computer to user
+    if (computerChoice === theKey) {
+        wins++;
+        console.log("Wins: " + wins);
+    } else {
+        guessesLeft--;
+        console.log("Guesses Left: " + guessesLeft);
+    };
+
 };
 
-var userKey = userGuess;
-
-// compare user and computer
-if (computerChoice === userKey) {
-    wins++;
-} else {
-    guessesLeft--;
-};
 
 
 
 
-console.log("Computer's Guess: " + computerChoice);
-console.log("Wins: " + wins);
+
+
 console.log("Losses: " + losses);
-console.log("Guesses Left: " + guessesLeft);
