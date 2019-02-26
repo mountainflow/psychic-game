ae94e1976a26ac30e73791e25ed9a8c011cfbf18
mountainflow <46331541+mountainflow@users.noreply.github.com>
2019-02-25 20:02:59 -0700

    still trying to get it so user can't use same key twice

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,7 +2,7 @@
 var wins = 0;
 var losses = 0;
 var guessesLeft = 9;
-
+var guessesSoFar = ["E"];
 
 // computer picks random letters
 function computerGuess() {
@@ -14,7 +14,6 @@ console.log("Computer's Guess: " + computerChoice);
 
 
 
-// NEED TO MAKE USER GUESS BETWEEN 65 AND 90 TO ONLY SELECT LETTERS
 // NEED TO MAKE IT SO NOTHING HAPPENS WHEN USER HITS THE SAME KEY MULTIPLE TIMES
 
 
@@ -24,49 +23,84 @@ document.addEventListener("keyup", userGuess);
 
 function userGuess() {
     var theKey = event.key.toUpperCase();
+    if (event.keyCode >= 65 && event.keyCode <= 90) {
 
-    // add the user guesses to html
-    var listItem = document.createElement("li");
-    var t = document.createTextNode(theKey + ", ");
-    listItem.appendChild(t);
-    document.getElementById("guesses").appendChild(listItem);
 
-    console.log("Your Guess: " + theKey);
+        var useGuess = true;
+        // var n = guessesSoFar.includes(theKey);
 
-    // compare computer to user
-    if (computerChoice === theKey) {
-        wins++;
-        computerChoice = String.fromCharCode(computerGuess());
-        guessesLeft = 9;
-        document.getElementById("wins").textContent = "Wins: " + wins;
-        document.getElementById("guessesLeft").textContent = "Guesses Left: " + guessesLeft;
+        if (guessesSoFar.includes(theKey)) {
+            userGuess = false;
+        }
 
-        // clearing the guesses
-        document.getElementById("guesses").textContent = "";
 
-        console.log("you win");
-        console.log("Wins: " + wins);
-        console.log("Computer's Guess: " + computerChoice);
-    } else {
-        guessesLeft--;
-        document.getElementById("guessesLeft").textContent = "Guesses Left: " + guessesLeft;
-        console.log("Guesses Left: " + guessesLeft);
+        // check guessesSoFar array for used keys
+        // if has been used
+        // useGuess = false;
+
+
+        console.log("keyCode ", event.key);
+
+        // put theKey inside guessesSoFar array
+
+
+
+
+        if (useGuess === true) {
+
+
+            // add the user guesses to html
+            var listItem = document.createElement("li");
+            var t = document.createTextNode(theKey + ", ");
+            listItem.appendChild(t);
+            document.getElementById("guesses").appendChild(listItem);
+
+            console.log("Your Guess: " + theKey);
+
+            // compare computer to user
+            if (computerChoice === theKey) {
+                wins++;
+                computerChoice = String.fromCharCode(computerGuess());
+                guessesLeft = 9;
+                document.getElementById("wins").textContent = "Wins: " + wins;
+                document.getElementById("guessesLeft").textContent = "Guesses Left: " + guessesLeft;
+                alert("AWESOME!!");
+
+                // clearing the guesses
+                document.getElementById("guesses").textContent = "";
+
+                console.log("you win");
+                console.log("Wins: " + wins);
+                console.log("Computer's Guess: " + computerChoice);
+            } else {
+                guessesLeft--;
+                document.getElementById("guessesLeft").textContent = "Guesses Left: " + guessesLeft;
+                console.log("Guesses Left: " + guessesLeft);
+            };
+
+            if (guessesLeft === 0) {
+                losses++;
+                computerChoice = String.fromCharCode(computerGuess());
+                guessesLeft = 9;
+                document.getElementById("losses").textContent = "Losses: " + losses;
+                document.getElementById("guessesLeft").textContent = "Guesses Left: " + guessesLeft;
+                alert("YOU SUCK AT THIS!!");
+
+                // clearing the guesses
+                document.getElementById("guesses").textContent = "";
+
+                console.log("you lose");
+                console.log("Losses: " + losses);
+                console.log("Computer's Guess: " + computerChoice);
+
+            };
+        } else {
+            alert("YOU HAVE ALREADY USED THAT LETTER!!");
+        };
+
+
     };
 
-    if (guessesLeft === 0) {
-        losses++;
-        computerChoice = String.fromCharCode(computerGuess());
-        guessesLeft = 9;
-        document.getElementById("losses").textContent = "Losses: " + losses;
-        document.getElementById("guessesLeft").textContent = "Guesses Left: " + guessesLeft;
 
-        // clearing the guesses
-        document.getElementById("guesses").textContent = "";
-
-        console.log("you lose");
-        console.log("Losses: " + losses);
-        console.log("Computer's Guess: " + computerChoice);
-
-    };
 
 };
